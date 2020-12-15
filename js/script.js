23f6e6ee15ed242753882570c75b422f9e10bf5a
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2020-12-15 16:04:43 +0100

    Inizio dadi

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 var mail = prompt('Benvenuto! Inserisci la tua mail, se sei regolarmente iscritto potrai giocare!');
-
+var gioco = document.getElementById('gioco');
+var cliccabile = document.getElementById('btn');
+var dadopg = document.getElementById('dado-pg');
+var dadopc = document.getElementById('dado-pc');
+var esito = document.getElementById('esito');
 var mailArray = ['nome@gmail.com', 'nome@hotmail.com', 'nome@yahoo.com'];
-
 var min = 1;
 var max = 6;
-var dado = Math.floor(Math.random() * (max + 1 - min) + min);
+var dado1 = Math.floor(Math.random() * (max + 1 - min) + min);
+var dado2 = Math.floor(Math.random() * (max + 1 - min) + min);
 
 for (var i = 0; i < mailArray.length; i++) {
   if(mailArray[i] === 'nome@gmail.com'){
-    alert('Gioca!')
+    presentazione.innerText = 'Giochiamo, allora!';
+    cliccabile.innerText = 'Clicca qui per lanciare!';
+    cliccabile.addEventListener('click',
+      function(){
+        dadopg.innerText = dado1;
+        dadopc.innerText = dado2;
+      })
+    if (dadopg > dadopc) {
+
+    }
   }
 }
